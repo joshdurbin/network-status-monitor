eb17a33c4c94260aea675a6da75ab544eda6a97d
Josh Durbin <durbinjo593@gmail.com>
2016-05-24 23:15:11 -0700

    Update ScrapeMotoSBStats.groovy

--- ScrapeMotoSBStats.groovy
+++ ScrapeMotoSBStats.groovy
@@ -1,5 +1,4 @@
 @Grapes([
-  @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2'),
   @Grab(group='org.jsoup', module='jsoup', version='1.8.1')
 ])
 
@@ -10,24 +9,16 @@ import org.jsoup.select.Elements
 import groovy.transform.Canonical
 import groovy.transform.ToString
 import groovy.json.JsonBuilder
-import groovyx.net.http.HTTPBuilder
-import static groovyx.net.http.Method.POST
-import static groovyx.net.http.ContentType.JSON
 
-// constants
 def noBreakSpaceUnicode = '\u00a0'
 def noBreakSpaceUnicodeReplacement = ''
 def jsoupSelectSkipHeaderQuery = 'td:gt(0)'
 
-// closures
 def cleanInput = { input ->
-
   input.replace(noBreakSpaceUnicode, noBreakSpaceUnicodeReplacement).trim()
 }
 
-// data model used by JSON API
 @Canonical
-@ToString(includeSuperProperties = true)
 class SignalStatus {
 
   List<DownstreamChannel> downstreamChannels
@@ -35,7 +26,6 @@ class SignalStatus {
 }
 
 @Canonical
-@ToString(includeSuperProperties = true)
 class BaseChannel {
 
   Integer channelId
@@ -53,7 +43,6 @@ class DownstreamChannel extends BaseChannel {
 }
 
 @Canonical
-@ToString(includeSuperProperties = true)
 class DownstreamChannelStats {
 
   Long totalUnerroredCodewords
@@ -67,38 +56,36 @@ class UpstreamChannel extends BaseChannel {
 
   Integer rangingServiceId
   Double symbolRate
-  Boolean raningStatusSuccessful
+  Boolean rangingStatusSuccessful
   String modulation
 }
 
-// load and parse the page
-//def surfboardCMSignalData = Jsoup.parse(new File("cmSignalData.htm"), "UTF-8", "http://192.168.100.1/")
-def surfboardCMSignalData = Jsoup.connect("http://192.168.100.1/cmSignalData.htm").get()
+def surfboardCMSignalData = Jsoup.connect('http://192.168.100.1/cmSignalData.htm').get()
 
 // get the three relevant tables
-def downstreamTableData = surfboardCMSignalData.select("table:has(th:contains(Downstream))")
-def upstreamTableData = surfboardCMSignalData.select("table:has(th:contains(Upstream))")
-def downstreamStats = surfboardCMSignalData.select("table:has(th:contains(Signal Stats))")
+def downstreamTableData = surfboardCMSignalData.select('table:has(th:contains(Downstream))')
+def upstreamTableData = surfboardCMSignalData.select('table:has(th:contains(Upstream))')
+def downstreamStats = surfboardCMSignalData.select('table:has(th:contains(Signal Stats))')
 
 // get downstream data attributes
-def downstreamChannelIds = downstreamTableData.select("tr:has(td:contains(Channel ID))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
-def downstreamFrequencies = downstreamTableData.select("tr:has(td:contains(Frequency))").select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('Hz') }.collect (cleanInput)
-def downstreamSignalToNoiseRatios = downstreamTableData.select("tr:has(td:contains(Signal to Noise Ratio))").select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('dB') }.collect (cleanInput)
-def downstreamModulation = downstreamTableData.select("tr:has(td:contains(Downstream Modulation))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
-def downstreamPowerLevels = downstreamTableData.select("tr:has(td:contains(Power Level))").select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('dBmV') }.collect (cleanInput)
+def downstreamChannelIds = downstreamTableData.select('tr:has(td:contains(Channel ID))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def downstreamFrequencies = downstreamTableData.select('tr:has(td:contains(Frequency))').select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('Hz') }.collect (cleanInput)
+def downstreamSignalToNoiseRatios = downstreamTableData.select('tr:has(td:contains(Signal to Noise Ratio))').select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('dB') }.collect (cleanInput)
+def downstreamModulation = downstreamTableData.select('tr:has(td:contains(Downstream Modulation))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def downstreamPowerLevels = downstreamTableData.select('tr:has(td:contains(Power Level))').select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('dBmV') }.collect (cleanInput)
 
-def downstreamStatsUnerroredCodewords = downstreamStats.select("tr:has(td:contains(Total Unerrored Codewords))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
-def downstreamStatsCorrectableCodewords = downstreamStats.select("tr:has(td:contains(Total Correctable Codewords))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
-def downstreamStatsUncorrectableCodewords = downstreamStats.select("tr:has(td:contains(Total Uncorrectable Codewords))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def downstreamStatsUnerroredCodewords = downstreamStats.select('tr:has(td:contains(Total Unerrored Codewords))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def downstreamStatsCorrectableCodewords = downstreamStats.select('tr:has(td:contains(Total Correctable Codewords))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def downstreamStatsUncorrectableCodewords = downstreamStats.select('tr:has(td:contains(Total Uncorrectable Codewords))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
 
 // get upstream data attributes
-def upstreamChannelIds = upstreamTableData.select("tr:has(td:contains(Channel ID))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
-def upstreamFrequencies = upstreamTableData.select("tr:has(td:contains(Frequency))").select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('Hz') }.collect (cleanInput)
-def upstreamRangingServiceIds = upstreamTableData.select("tr:has(td:contains(Ranging Service ID))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
-def upstreamSymbolRates = upstreamTableData.select("tr:has(td:contains(Symbol Rate))").select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('Msym/sec') }.collect (cleanInput)
-def upstreamPowerLevels = upstreamTableData.select("tr:has(td:contains(Power Level))").select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('dBmV') }.collect (cleanInput)
-def upstreamModulation = upstreamTableData.select("tr:has(td:contains(Upstream Modulation))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
-def upstreamRangingStatusSuccessful = upstreamTableData.select("tr:has(td:contains(Ranging Status))").select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput).collect { it.equals('Success') ? true : false }
+def upstreamChannelIds = upstreamTableData.select('tr:has(td:contains(Channel ID))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def upstreamFrequencies = upstreamTableData.select('tr:has(td:contains(Frequency))').select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('Hz') }.collect (cleanInput)
+def upstreamRangingServiceIds = upstreamTableData.select('tr:has(td:contains(Ranging Service ID))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def upstreamSymbolRates = upstreamTableData.select('tr:has(td:contains(Symbol Rate))').select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('Msym/sec') }.collect (cleanInput)
+def upstreamPowerLevels = upstreamTableData.select('tr:has(td:contains(Power Level))').select(jsoupSelectSkipHeaderQuery).collect { it.text().minus('dBmV') }.collect (cleanInput)
+def upstreamModulation = upstreamTableData.select('tr:has(td:contains(Upstream Modulation))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput)
+def upstreamRangingStatusSuccessful = upstreamTableData.select('tr:has(td:contains(Ranging Status))').select(jsoupSelectSkipHeaderQuery).collect { it.text() }.collect (cleanInput).collect { it.equals('Success') ? true : false }
 
 def downstreamChannels = []
 
@@ -129,7 +116,7 @@ def upstreamChannels = []
     rangingServiceId: upstreamRangingServiceIds.get(counter) as Integer,
     symbolRate: upstreamSymbolRates.get(counter) as Double,
     modulation: upstreamModulation.get(counter),
-    raningStatusSuccessful: upstreamRangingStatusSuccessful.get(counter)
+    rangingStatusSuccessful: upstreamRangingStatusSuccessful.get(counter)
   ))
 }
 
@@ -138,19 +125,3 @@ def compiledSignalStatus = new SignalStatus(
   upstreamChannels: upstreamChannels)
 
 println new JsonBuilder(compiledSignalStatus).toPrettyString()
-
-/*
-def http = new HTTPBuilder( 'http://api.thingspace.com' )
-
-http.request(POST, JSON) { req ->
-
-  body = new JsonBuilder(compiledSignalStatus).toString()
-
-  response.success = { resp, json ->
-    println "Success! ${resp}"
-  }
-
-  response.failure = { resp ->
-    println "Ugh. Failure. ${resp}"
-  }
-}*/
